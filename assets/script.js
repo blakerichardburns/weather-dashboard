92ac1014a37fbfbd1f6fd2c1d9aaa817cdbba062
Blake Burns <bburns@mysgi.com>
2022-10-08 23:51:01 -0400

    added full html content, added some javascript variables, wrote out url paths for fetching city data, then lat&long data based on city data

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var weatherApiKey = "558c0603a2078d8a70f4729133b3fa07";
+var searchInput = "";
+var cityName = "";
+var latitude = "";
+var longitude = "";
+var searchHistory = [];
+
+
+
+
+"http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + weatherApiKey
+
+
+"https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + weatherApiKey
