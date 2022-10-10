4cb31591cbe8891787eb93276e2e0377bbe66976
Blake Burns <bburns@mysgi.com>
2022-10-09 20:11:31 -0400

    created fetch function for cityname, created fetch function to convert cityname to longitude and latitude, created variables for fetch data temp, humidity & windspeed for today and each of next five days

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,56 @@
 var weatherApiKey = "558c0603a2078d8a70f4729133b3fa07";
 var searchInput = "";
 var cityName = "";
-var latitude = "";
-var longitude = "";
 var searchHistory = [];
 
+function cityCoordinates() {
+    cityName = "Indianapolis";
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + weatherApiKey)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        var cityArray = data[0];
+        var latitude = cityArray.lat;
+        var longitude = cityArray.lon;
+        
+        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + weatherApiKey)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var todayArray = data.list[0];
+            var todayTemp = todayArray.main.temp;
+            var todayHumid = todayArray.main.humidity;
+            var todayWind = todayArray.wind.speed;
 
+            var futureOneArray = data.list[1];
+            var futureOneTemp = futureOneArray.main.temp;
+            var futureOneHumid = futureOneArray.main.humidity;
+            var futureOneWind = futureOneArray.wind.speed;
+            
+            var futureTwoArray = data.list[2];
+            var futureTwoTemp = futureTwoArray.main.temp;
+            var futureTwoHumid = futureTwoArray.main.humidity;
+            var futureTwoWind = futureTwoArray.wind.speed;
+            
+            var futureThreeArray = data.list[3];
+            var futureThreeTemp = futureThreeArray.main.temp;
+            var futureThreeHumid = futureThreeArray.main.humidity;
+            var futureThreeWind = futureThreeArray.wind.speed;
+            
+            var futureFourArray = data.list[4];
+            var futureFourTemp = futureFourArray.main.temp;
+            var futureFourHumid = futureFourArray.main.humidity;
+            var futureFourWind = futureFourArray.wind.speed;
+            
+            var futureFiveArray = data.list[5];
+            var futureFiveTemp = futureFiveArray.main.temp;
+            var futureFiveHumid = futureFiveArray.main.humidity;
+            var futureFiveWind = futureFiveArray.wind.speed;         
 
+        })
+    })
+}
 
-"http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + weatherApiKey
-
-
-"https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + weatherApiKey
+cityCoordinates();
