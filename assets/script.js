b7eb17f6e8d4963d9cec316f2059a80101680a80
Blake Burns <bburns@mysgi.com>
2022-10-10 23:21:26 -0400

    temp returned from api now converting from kelvin to farenheit, added javascript function for generating search history list that is not yet functioning correctly at all

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var weatherApiKey = "558c0603a2078d8a70f4729133b3fa07";
+var searchHistoryDisplay = document.querySelector("#search-history");
 var searchButton = document.querySelector("#search-button");
-var searchHistoryList = document.querySelector("#search-history");
 var cityNameDisplay = document.querySelector("#city-name");
 
 
@@ -39,15 +39,27 @@ var future3Icon = document.querySelector("#future-icon-3");
 var future4Icon = document.querySelector("#future-icon-4");
 var future5Icon = document.querySelector("#future-icon-5");
 
-var searchHistory = localStorage.getItem("city");
-// searchHistoryList.appendChild(searchHistory);
+// if (savedSearches.length != "null") {
+//     searchHistory ();
+// }
 
+var searchInputArray = [];
+// var savedSearches = JSON.parse(localStorage.getItem("cityList"));
+// function searchHistory () {
+//     for (i = 0; i < savedSearches.length; i++) {
+//     var searchHistoryEl = document.createElement("li");
+//     searchInputArray.push(savedSearches[i]);
+//     searchHistoryEl.textContent = searchInputArray[i];
+//     searchHistoryDisplay.appendChild(searchHistoryEl);
+// }}
 
 searchButton.addEventListener("click", function(event) {
     event.preventDefault();
-    var searchInput = document.querySelector("#search-input").value;
 
-    localStorage.setItem("city", searchInput);
+    var searchInput = document.querySelector("#search-input").value;
+    
+    searchInputArray.push(searchInput);
+    localStorage.setItem("cityList", JSON.stringify(searchInputArray));
 
     cityCoordinates();    
     
@@ -71,37 +83,37 @@ searchButton.addEventListener("click", function(event) {
             var cityName = data.city.name;
 
             var todayArray = data.list[0];
-            var todayTemp = todayArray.main.temp;
+            var todayTemp = Math.trunc((todayArray.main.temp - 273.1) * 1.8 + 32);
             var todayHumid = todayArray.main.humidity;
             var todayWind = todayArray.wind.speed;
             // var todayIconValue = todayArray.weather.main
             
             var futureOneArray = data.list[1];
-            var futureOneTemp = futureOneArray.main.temp;
+            var futureOneTemp = Math.trunc((futureOneArray.main.temp - 273.1) * 1.8 + 32);
             var futureOneHumid = futureOneArray.main.humidity;
             var futureOneWind = futureOneArray.wind.speed;
             // var futureOneIconValue = futureOneArray.weather.main 
             
             var futureTwoArray = data.list[2];
-            var futureTwoTemp = futureTwoArray.main.temp;
+            var futureTwoTemp = Math.trunc((futureTwoArray.main.temp - 273.1) * 1.8 + 32);
             var futureTwoHumid = futureTwoArray.main.humidity;
             var futureTwoWind = futureTwoArray.wind.speed;
             // var futureTwoIconValue = futureTwoArray.weather.main 
             
             var futureThreeArray = data.list[3];
-            var futureThreeTemp = futureThreeArray.main.temp;
+            var futureThreeTemp = Math.trunc((futureThreeArray.main.temp - 273.1) * 1.8 + 32);
             var futureThreeHumid = futureThreeArray.main.humidity;
             var futureThreeWind = futureThreeArray.wind.speed;
             // var futureThreeIconValue = futureThreeArray.weather.main 
             
             var futureFourArray = data.list[4];
-            var futureFourTemp = futureFourArray.main.temp;
+            var futureFourTemp = Math.trunc((futureFourArray.main.temp - 273.1) * 1.8 + 32);
             var futureFourHumid = futureFourArray.main.humidity;
             var futureFourWind = futureFourArray.wind.speed;
             // var futureFourIconValue = futureFourArray.weather.main 
             
             var futureFiveArray = data.list[5];
-            var futureFiveTemp = futureFiveArray.main.temp;
+            var futureFiveTemp = Math.trunc((futureFiveArray.main.temp - 273.1) * 1.8 + 32);
             var futureFiveHumid = futureFiveArray.main.humidity;
             var futureFiveWind = futureFiveArray.wind.speed;
             // var futureFiveIconValue = futureFiveArray.weather.main 
@@ -128,12 +140,12 @@ searchButton.addEventListener("click", function(event) {
             var future5Date = moment().add(5, 'days').format("ddd MM/DD/YY");
             future5DateDisplay.textContent = future5Date;
 
-            todayTempDislay.textContent = "Today's Temp: " + todayTemp
-            future1TempTempDislay.textContent = "Temp: " + futureOneTemp
-            future2TempTempDislay.textContent = "Temp: " + futureTwoTemp
-            future3TempTempDislay.textContent = "Temp: " + futureThreeTemp
-            future4TempTempDislay.textContent = "Temp: " + futureFourTemp
-            future5TempTempDislay.textContent = "Temp: " + futureFiveTemp
+            todayTempDislay.textContent = "Today's Temp: " + todayTemp + "° Fahrenheit"
+            future1TempTempDislay.textContent = "Temp: " + futureOneTemp + "°F"
+            future2TempTempDislay.textContent = "Temp: " + futureTwoTemp + "°F"
+            future3TempTempDislay.textContent = "Temp: " + futureThreeTemp + "°F"
+            future4TempTempDislay.textContent = "Temp: " + futureFourTemp + "°F"
+            future5TempTempDislay.textContent = "Temp: " + futureFiveTemp + "°F"
             
             todayHumidDisplay.textContent = "Today's Humidity: " + todayHumid
             future1HumidDisplay.textContent = "Humidity: " + futureOneHumid
